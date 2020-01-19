789b5124ff24d1c35d20c97ca7b4d53fc4363a20
JSchrtke <johannes.schroetke@gmail.com>
2020-01-19 17:05:52 +0100

    Move CalculatorTest into proper directory

---
+++ src/calculatortest/CalculatorTest.java
@@ -0,0 +1,32 @@
+package calculatortest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import calculator.Calculator;
+
+public class CalculatorTest {
+	private void calc(String input, double expected) {
+		Calculator c = new Calculator();
+		c.evaluateString(input);
+		assertEquals(expected, c.getResult(), 1e-6);
+	}
+
+	@Test
+	public void emptyInputIsIgnored() {
+		calc("", 0);
+	}
+
+	@Test
+	public void singleNumberIsSimplyEvaluated() {
+		calc("12.25", 12.25);
+	}
+
+	@Test
+	public void divisionByZeroIsError() {
+		calc("1/0", 0);
+		// TODO output is always a double, Calculator does not even address invalid
+		// input
+	}
+}
